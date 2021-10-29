235eec7f4b1423b843980dac3cfd9f3193162a47
nagrohan726 <nag.rohan726@gmail.com>
2021-10-29 09:51:11 +0530

    Lights

---
+++ Lights.java
@@ -0,0 +1,31 @@
+package basic;
+
+class Lamp
+{
+	boolean isOn;
+
+	void turnOn(String bulb)
+	{
+		isOn = true;
+		System.out.println(bulb + " on? " + isOn);
+	}
+
+	void turnOff(String bulb)
+	{
+		isOn = false;
+		System.out.println(bulb + " on? " + isOn);
+	}
+}
+
+public class Lights
+{
+	public static void main(String args[])
+	{
+		Lamp led = new Lamp();
+		Lamp halogen = new Lamp();
+
+		led.turnOn("Led");
+
+		halogen.turnOff("Halogen");
+	}
+}
