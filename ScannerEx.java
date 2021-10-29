dac25c29987dc30edd1131b04590628b1b0cd432
nagrohan726 <nag.rohan726@gmail.com>
2021-10-29 09:33:26 +0530

    Scanner Example

---
+++ ScannerEx.java
@@ -0,0 +1,17 @@
+package basic;
+
+import java.util.Scanner;
+
+public class ScannerEx
+{
+	public static void main(String[] args)
+	{
+		Scanner reader = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+
+		int number = reader.nextInt();
+
+		System.out.println("You entered " + number);
+
+	}
+}
