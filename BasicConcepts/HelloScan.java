99a0bcac92820be4c6603a4f6269cb0bafe62629
nagrohan726 <nag.rohan726@gmail.com>
2021-11-12 14:57:18 +0530

    First IntelliSence Experience

---
+++ BasicConcepts/HelloScan.java
@@ -0,0 +1,19 @@
+package basic;
+
+import java.util.Scanner;
+/**
+ * HelloScan
+ */
+public class HelloScan {
+
+    public static void main(String[] args) {
+        
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter Your Name: ");
+        String name = reader.nextLine();
+
+        System.out.println("Hello " + name +".");
+        
+    }
+}
