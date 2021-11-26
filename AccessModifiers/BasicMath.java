9f984a1964c3c55cc42bddde36dfe1ba1523b29f
nagrohan726 <nag.rohan726@gmail.com>
2021-11-26 10:08:26 +0530

    Nov, 26 class until break at 10

---
+++ AccessModifiers/BasicMath.java
@@ -0,0 +1,9 @@
+package Pack1;
+
+public class BasicMath {
+
+    public int add (int num1, int num2){
+
+        return (num1 + num2);
+    }
+}
---
+++ AccessModifiers/EntryPoint.java
@@ -0,0 +1,13 @@
+package Pack1;
+
+public class EntryPoint {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello Everyone!");
+
+        BasicMath math = new BasicMath();
+        System.out.println(math.add(10,20));
+    }
+    
+}
