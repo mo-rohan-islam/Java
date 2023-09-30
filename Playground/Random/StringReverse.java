aefd745fba5a42851f5e581c85ad57c5956d4c9a
mo-rohan-islam <mo.rohan.islam@gmail.com>
2023-09-30 21:58:43 +0530

    Coding practise

---
+++ Playground/Random/StringReverse.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@SuppressWarnings(value = { "unused" })
+public class StringReverse {
+    public static void main(String[] args) {
+
+        final String SPACE_REGEX = "\\s+";
+        final char SPACE = ' ';
+
+        String str = "How are you doing today?";
+        // String palindromeInput = "Madam";
+        String strReversed = "";
+
+        // Expected Output: "woh are uoy doing ?yadot"
+
+        // List<String> splitSringList = splitStringByRegex(str, SPACE_REGEX);
+        List<String> splitSringList = splitStringByChar(str, SPACE);
+
+        for (int i = 0; i < splitSringList.size(); i++) {
+            String word = splitSringList.get(i);
+            strReversed += ((i % 2 == 0) ? reverseString(word) : word) + SPACE;
+        }
+
+        // strReversed = reverseString(palindromeInput);
+        // System.out.println("Is Palindrome? " +
+        // checkPalindromeInsensitive(palindromeInput, strReversed));
+
+        System.out.println(strReversed);
+    }
+
+    private static List<String> splitStringByChar(String input, char delimeter) {
+        input = input.trim();
+        List<String> splitStringList = new ArrayList<>();
+
+        String word = "";
+        int lastIndex = input.length() - 1;
+        for (int i = 0; i <= lastIndex; i++) {
+            char ch = input.charAt(i);
+            if (ch == delimeter || i == lastIndex) {
+                if (i == lastIndex) {
+                    word += ch;
+                }
+                splitStringList.add(word);
+                word = "";
+            } else {
+                word += ch;
+            }
+        }
+
+        return splitStringList;
+    }
+
+    private static List<String> splitStringByRegex(String input, String delimeter) {
+        String[] words = input.split(delimeter);
+        List<String> splitStringList = Arrays.asList(words);
+        return splitStringList;
+    }
+
+    private static String reverseString(String str) {
+        String rev = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+        return rev;
+    }
+
+    private static Boolean checkPalindromeInsensitive(String input, String reverse) {
+        boolean result = true;
+        input = input.toLowerCase();
+        reverse = reverse.toLowerCase();
+        for (int i = 0; i < input.length(); i++) {
+            char inputChar = input.charAt(i);
+            char reverseChar = reverse.charAt(i);
+            System.out.println((int) inputChar + " - " + (int) reverseChar);
+            if ((int) inputChar != (int) reverseChar) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
---
+++ Playground/Random/Teenager.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Teenager {
+    public static void main(String[] args) {
+        // write a program to fetch teenager customers from list of customers
+
+        List<Customer> listOfCustomers = new ArrayList<>();
+        listOfCustomers.add(new Customer(12, "Rohan"));
+        listOfCustomers.add(new Customer(13, "Sohan"));
+        listOfCustomers.add(new Customer(20, "Mohan"));
+        listOfCustomers.add(new Customer(19, "Sriti"));
+        listOfCustomers.add(new Customer(15, "Shrusti"));
+
+        listOfCustomers.stream().filter(customer -> customer.getAge() >= 13 && customer.getAge() <= 19)
+                .forEach(System.out::println);
+
+        System.out.println();
+
+        List<String> adultNames = listOfCustomers.stream()
+                .sorted(Comparator.comparingInt(Customer::getAge).reversed())
+                .filter(customer -> customer.getAge() >= 18)
+                .peek(System.out::println)
+                .map(Customer::getName).toList();
+        System.out.println(adultNames);
+    }
+}
+
+class Customer {
+    int age;
+    String name;
+
+    @Override
+    public String toString() {
+        return "Customer [age=" + age + ", name=" + name + "]";
+    }
+
+    public Customer(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
